4c7013c5c4e13c6e08dc75cf728209151bd4f8e9
metinogurlu <a.metinugurlu@gmail.com>
2020-08-18 13:58:54 +0300

    remove bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 import Head from 'next/head'
 import styles from './index.module.css'
 import Header from '../components/Header/header.js'
-import { Container, Row, Col, Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
 
 const Home = () => (
-    <Container>
-      <Row className={styles.fullwidth}>
-        <Col className={`col-8 ${styles.mainContent}`}>
-          <Header/>
-        </Col>
-        <Col className={`col-4 ${styles.sidebar}`}>
-          <Header/>
-        </Col>
-      </Row>
-    </Container>
+  <></>
 )
 
 export default Home
